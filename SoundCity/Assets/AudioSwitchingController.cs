90d61d2dd6574aab8e11cbc2c7c6a80dc479ebdb
0Poschi0 <patricktemborius@gmail.com>
2018-05-24 20:10:14 +0200

    Updated

---
+++ SoundCity/Assets/AudioSwitchingController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwitchingController : MonoBehaviour {
+
+    public void switchTimeTo(int time){
+        GlobalVariables.time = time;
+    }
+}
