842f7d9faec6c0bff1722a4634a6b7a49a0afa2a
0Poschi0 <patricktemborius@gmail.com>
2018-05-24 15:19:05 +0200

    First Commit

---
+++ SoundCity/Assets/AudioSwitcher.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwitcher : MonoBehaviour {
+
+    int currTime = 1;
+
+    public AudioSource[] audiosources;
+
+	// Use this for initialization
+	void Start () {
+        currTime = GlobalVariables.time;
+
+        for(int i=0; i < audiosources.Length; i++)
+        {
+            if (i != currTime)
+            {
+                audiosources[i].mute = true;
+            }
+            else
+            {
+                audiosources[i].mute = false;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (currTime != GlobalVariables.time)
+        {
+            currTime = GlobalVariables.time;
+            for (int i = 0; i < audiosources.Length; i++)
+            {
+                if (i != currTime)
+                {
+                    audiosources[i].mute = true;
+                }
+                else
+                {
+                    audiosources[i].mute = false;
+                }
+            }
+        }
+
+	}
+}
---
+++ SoundCity/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 inputVec = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+
+        if (inputVec.magnitude < 0.2f)
+        {
+            rb.velocity = new Vector2();
+            return;
+        }
+
+        Vector3 directionPos = Camera.main.WorldToScreenPoint(transform.position + new Vector3(inputVec.x, inputVec.y));
+        Vector2 object_pos = Camera.main.WorldToScreenPoint(transform.position);
+        directionPos.x -= object_pos.x;
+        directionPos.y -= object_pos.y;
+        float angle = Mathf.Atan2(directionPos.y, directionPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle-90));
+
+        if (inputVec.magnitude > 1) inputVec = inputVec.normalized;
+        rb.velocity = inputVec * speed;
+	}
+}
