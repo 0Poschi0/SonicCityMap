9e2333984b71e5166209d6495733e9e514be5c81
MINIMERRY\Fluxilis <felixschoeller95@gmail.com>
2018-05-26 18:22:51 +0200

    Street Musician Done (in my WIP scene)
    -made particle child of street musician game object
    -also removed second street musician (+particles)
    -wrote an audio switcher script based on patrick's, which can handle multiple sounds per timeperiod

---
+++ SoundCity/Assets/AudioSwitcherMultiple.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwitcherMultiple : MonoBehaviour {
+
+    int currTime = 1;
+
+    public AudioSource[] pastSources;
+    public AudioSource[] presentSources;
+
+    // Use this for initialization
+    void Start()
+    {
+        currTime = GlobalVariables.time;
+
+        SetTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (currTime != GlobalVariables.time)
+        {
+            currTime = GlobalVariables.time;
+
+            SetTime();
+        }
+
+    }
+
+    void SetTime()
+    {
+        bool setPast = currTime == 1 ? true : false;
+
+        for (int i = 0; i < pastSources.Length; i++)
+        {
+            pastSources[i].mute = setPast;
+        }
+
+        for (int i = 0; i < presentSources.Length; i++)
+        {
+            presentSources[i].mute = !setPast;
+        }
+    }
+}
